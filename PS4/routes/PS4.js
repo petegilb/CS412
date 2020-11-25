c3233087b3885ef5bb78d6fad9c08ffa27197e84
Peter Gilbert <petegilb@bu.edu>
2020-11-24 19:04:32 -0500

    ps5b

--- PS4/routes/PS4.js
+++ PS4/routes/PS4.js
@@ -3,6 +3,20 @@ const router = express.Router();
 
 const fetch = require('node-fetch');
 const fetchConfig = require('../config/fetchConfig');
+
+const redis = require('redis');
+const client = redis.createClient();
+const {promisify} = require('util');
+
+const asyncSet = promisify(client.set).bind(client);
+const asyncGet = promisify(client.get).bind(client);
+const asyncExists = promisify(client.exists).bind(client);
+const asyncExpire = promisify(client.expire).bind(client);
+
+client.flushdb((err, response) => {
+    if (err) {throw new Error('bad stuff happened when flushing')}
+});
+
 //uses fetch to grab definitions from the Oxford Dictionaries API
 router.route('/')
     .get((req, res) => {
@@ -10,24 +24,36 @@ router.route('/')
     })
     //route is a post and uses the get method with the oxford dictionary with a word input to retrieve its data
     .post(async (req, res) => {
-        let word;
-        //if there is an inputted word from the pug template
-        if (req.body.wordRequest) {
-            word = req.body.wordRequest;
-        }
-        //otherwise find the definition of a sword
-        else
-            word = "sword";
-        let rawReturnValue = await fetch(fetchConfig.fetchOptions.url + word + "?fields=definitions&strictMatch=false", {
-            method: 'GET',
-            headers: {"app_id": fetchConfig.fetchOptions.id, "app_key": fetchConfig.fetchOptions.key}
-        });
-        const cleanReturnValue = await rawReturnValue.json();
-        //this json file has so many variables for some reason so I just got the first definition of the word I could find
-        try {
-            res.render("PS4", {results: cleanReturnValue["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0]});
-        } catch(error) {
-            res.render("PS4", {results: "definition not found"});
+        //if there is an inputted word from the pug template otherwise lookup sword
+        let word = (req.body.wordRequest ? req.body.wordRequest: 'sword');
+        let match = await asyncExists(word);
+        //if the word is in the cache
+        if (match){
+            let def = await asyncGet(word);
+            let response = {
+                definition: def,
+                cached: true
+            }
+            res.send(response);
+        } else { //otherwise call the api
+            let rawReturnValue = await fetch(fetchConfig.fetchOptions.url + word + "?fields=definitions&strictMatch=false", {
+                method: 'GET',
+                headers: {"app_id": fetchConfig.fetchOptions.id, "app_key": fetchConfig.fetchOptions.key}
+            });
+            const cleanReturnValue = await rawReturnValue.json();
+            try { //if there was a definition found
+                let def = cleanReturnValue["results"][0]["lexicalEntries"][0]["entries"][0]["senses"][0]["definitions"][0];
+                await asyncSet(word, def);
+                let response = {
+                    definition: def,
+                    cached: false
+                }
+                await asyncExpire(word, 15); //expire after 15 seconds
+                res.send(response);
+            }
+            catch (error){
+                res.send("definition not found");
+            }
         }
     })
 
